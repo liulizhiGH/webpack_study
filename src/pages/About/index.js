20678f9cbb981254278f1a999f9c473fb06942e5
liulizhiGH <liulizhiasd777@163.com>
2019-11-10 22:46:58 +0800

    安装mobx，mobx-react,moment

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import './style.less';
+import React, { Component } from "react";
+import "./style.less";
+import  moment from 'moment'
 
 export default class index extends Component {
   render() {
     return (
       <div id="about">
         关于我们
-        <h1>welcome dev修改</h1>
+        <h1>welcome dev修改{moment().format("YYYY-MM-DD")}</h1>
       </div>
-    )
+    );
   }
 }
